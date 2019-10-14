d887016b805ddd05ec1ec2f63d807dde161baf82
Wesley Matos <wesley.matos@redfox.tech>
2019-10-14 02:17:14 -0300

    persist redux store

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Box } from '@material-ui/core';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import DefaultScreen from './screens';
-import store from './store';
+import Store from './store';
 
 const App = () => {
+  const { store, persistor } = Store;
+
   return (
     <div>
       <Provider store={store}>
-        <DefaultScreen />
+        <PersistGate
+          persistor={persistor}
+        >
+          <Box
+            display="flex"
+            justifyContent="center"
+            height="100vh"
+          >
+            <DefaultScreen />
+          </Box>
+        </PersistGate>
       </Provider>
       <ToastContainer />
     </div>
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,29 @@
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import auth from './auth';
+import alerts from './alerts';
+
+const persistConfig = {
+  key: 'test_rankmyapp',
+  storage,
+};
 
 const reducer = combineReducers({
+  alerts,
   auth,
 });
 
-export default createStore(
-  reducer,
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = createStore(
+  persistedReducer,
   process.env.NODE_ENV === 'development'
     && window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+const persistor = persistStore(store)
+
+export default { store, persistor };
