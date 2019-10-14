abaa3b55e902c80ba6fbd6b59e6cfee3eb7cc6f1
Wesley Matos <wesley.matos@redfox.tech>
2019-10-14 03:16:32 -0300

    add button to logout and alerts screen

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import Login from './Login'
+import Login from './Login';
+import Alerts from './Alerts';
+import { Button, Box } from '@material-ui/core';
+import { logout } from '../store/auth/actions';
 
 class Screen extends Component {
   render() {
-    const { token } = this.props;
+    const { token, dispatch } = this.props;
 
     return (
       <div>
         {
-          token ? <div>aa</div> : <Login />        
+          token
+            ? (
+              <div>
+                <Alerts />
+                <Box
+                  position="absolute"
+                  right="30px"
+                  bottom="30px"
+                >
+                  <Button
+                    onClick={() => dispatch(logout())}
+                  >
+                    Logout
+                  </Button>
+                </Box>
+              </div>
+            )
+            : <Login />        
         }
       </div>
     );
