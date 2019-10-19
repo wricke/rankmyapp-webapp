4418e27caac4b6b56172da76bf7dd529865b0f03
Wesley Matos <wesley.matos@redfox.tech>
2019-10-19 13:27:35 -0300

    call api to remove alert and remove from redux store

--- src/components/Alerts/Item/index.jsx
+++ src/components/Alerts/Item/index.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import { ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
-import { Mail } from '@material-ui/icons';
+import React, { Component } from 'react';
+import { ListItem, ListItemText, ListItemIcon, IconButton } from '@material-ui/core';
+import { Mail, Delete } from '@material-ui/icons';
+import { connect } from 'react-redux';
+import { toast } from 'react-toastify';
+import api from '../../../services/api';
+import { remove } from '../../../store/alerts/actions';
 
-const Alert = ({ alert }) => {
-  const { email, time, keywords } = alert;
+class Alert extends Component {
+  removeAlert = async (_id) => {
+    const { dispatch, auth } = this.props
 
-  return (
-    <ListItem>
-      <ListItemIcon>
-        <div>
-          <Mail />
-        </div>
-      </ListItemIcon>
-      <ListItemText
-        primary={email}
-        secondary={`${time} ${keywords}`}
-      />
-    </ListItem>
-  )
+    try {
+      await api.delete(`alert/${_id}`, { headers: { Authorization: `Bearer ${auth.token}` }});
+
+      dispatch(remove(_id))
+    } catch (error) {
+      const { data } = error.response;
+      toast.error(data.message);
+    }
+  }
+
+  render() {
+    const { _id, email, time, keywords } = this.props.alert;
+
+    return (
+      <ListItem>
+        <ListItemIcon>
+          <div>
+            <Mail />
+          </div>
+        </ListItemIcon>
+        <ListItemText
+          primary={email}
+          secondary={`${time} ${keywords}`}
+        />
+        <IconButton onClick={() => this.removeAlert(_id)}>
+          <Delete />
+        </IconButton>
+      </ListItem>
+    )
+  }
 };
 
-export default Alert;
+const mapStateToProps = state => state
+
+export default connect(
+  mapStateToProps,
+)(Alert);
+
